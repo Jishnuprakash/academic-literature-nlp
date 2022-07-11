e2a85c5305ef77f96ba8928c415cc1be54755136
Kunnanath Poduvattil, Jishnu Prakash <jishnuprakash.kp@gmail.com>
2022-07-11 19:31:46 +0100

    nltk

--- handover.py
+++ handover.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # import seaborn as sns
-import nltk
+# import nltk
 from nltk import word_tokenize
 from nltk.probability import FreqDist
 from matplotlib import pyplot as plt
